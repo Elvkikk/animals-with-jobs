2c84896b58e1be2591a7b2d3f130b4c9a5ce43a6
elvkikk <elviskika19@outlook.com>
2026-04-17 04:19:21 +0200

    fetched datt from Animals.ts

--- src/modules/fetchAnimals.ts
+++ src/modules/fetchAnimals.ts
@@ -2,4 +2,23 @@
 //Använd try/catch och om anropet lyckas, returnera datan. Annars, returnera "null"
 //Typa upp funktionen med vad den returnerar
 
-export default async function fetchAnimals() {}
+
+import { Animal } from "./IAnimal";
+
+// async function retunerar inte datan direkt, utan en Promise som kommer att innehålla datan när den är tillgänglig.
+export default async function fetchAnimals(): Promise<Animal[] | null> {
+    try {
+        const response = await fetch('/data/data.json') ;
+        if (!response.ok) {
+            throw new Error(`Request error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Karaktärerna har inte kunnat hämtas...:', error);
+        return null;
+    }
+}
+
+// Håller den här funktionen minimalistisk och fokuserad på att bara hämta data, så att den är lätt att återanvända och underhålla. 
+// Ett exemppel från MDN: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#fetching_json_data    
