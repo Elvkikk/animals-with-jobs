cec6b99a919739127b4975201afcba57b1950d69
elvkikk <elviskika19@outlook.com>
2026-04-17 03:55:30 +0200

    created interface from data.json

--- src/modules/IAnimal.ts
+++ src/modules/IAnimal.ts
@@ -1,1 +1,10 @@
-export interface Animal {}
+export interface Animal {
+    name: string;
+    kindOfAnimal: string;
+    job: string;
+    skills: string[]; // Det får bli en array av strängar som beskriver karaktärens färdigheter
+    birthYear: number;
+    imageURL: string; // En bild av karaktären
+    employmentStartDate: string; // Startdatum 
+    employmentEndDate: string; // Slutdatum 
+}
